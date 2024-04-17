91e5a0e6d526ff06641c50fb00e0d811518ad1d0
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2024-04-17 21:14:30 +0200

    chore: use sepolia in test connector

--- tests/getTestConnectors.ts
+++ tests/getTestConnectors.ts
@@ -1,6 +1,6 @@
 import { createPublicClient, http, createWalletClient } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
-import { goerli } from 'viem/chains'
+import { sepolia } from 'viem/chains'
 
 const privKey = process.env.TEST_PRIVATE_KEY as `0x${string}` | undefined
 
@@ -9,14 +9,14 @@ if (!privKey) throw new Error('Error: please add priv key to .env')
 export const getTestConnectors = () => {
   // Public Client: This is used to read from the blockchain.
   const publicClient = createPublicClient({
-    chain: goerli,
+    chain: sepolia,
     transport: http(),
   })
 
   // Wallet Client used to write data to the blockchain
   const walletClient = createWalletClient({
     account: privateKeyToAccount(privKey),
-    chain: goerli,
+    chain: sepolia,
     transport: http(),
   })
 
