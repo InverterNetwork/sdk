bdde91476f2598a29e79bbc92c8da4226b05418f
mguleryuz <mguleryuz3@gmail.com>
2024-04-23 14:24:48 +0300

    chore: q polish

--- src/getModule/parseInputs/index.ts
+++ src/getModule/parseInputs/index.ts
@@ -2,14 +2,12 @@ import { FormattedAbiParameter } from '../../types/parameter'
 import { Extras } from '../../types/base'
 import parse from './parse'
 
-export default function parsedInputs(
+export default function parseInputs(
   inputsProp: any,
-  argsProp: any,
+  args: any,
   extras?: Extras
 ) {
-  const inputs = inputsProp as FormattedAbiParameter[],
-    args = argsProp as any[]
-
+  const inputs = inputsProp as FormattedAbiParameter[]
   // parse the inputs
   const parsedInputs = inputs.map((input, index) => {
     // get the argument of the same index
