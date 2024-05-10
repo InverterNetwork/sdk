f9454d6ca448b2d61f2775a029e09fffe7987b22
fabianschu <48454910+fabianschu@users.noreply.github.com>
2024-05-10 21:00:38 +0200

    chore: test boilerplate for external decimals

--- tests/utils/parseInputs.test.ts
+++ tests/utils/parseInputs.test.ts
@@ -6,9 +6,46 @@ import { getTestConnectors } from '../getTestConnectors'
 describe('#parseInputs', () => {
   const { publicClient, walletClient } = getTestConnectors()
 
+  const USDC_SEPOLIA = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238' // USDC has 6 decimals
+
   describe('with decimals tag', () => {
+    describe.only('with :external', () => {
+      const args = ['42069']
+      const formattedInputs = [
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'minimumPayoutAmount',
+          type: 'uint256',
+          tags: ['decimals:external:exact:fundingManager'],
+          description:
+            'The minimum amount of tokens the Bounty will pay out upon being claimed',
+        },
+      ]
+      const extras = {
+        walletAddress:
+          '0x86fda565A5E96f4232f8136141C92Fd79F2BE950' as `0x${string}`,
+      }
+
+      let minimumPayoutAmount
+
+      beforeEach(async () => {
+        ;[minimumPayoutAmount] = await parseInputs({
+          formattedInputs,
+          args,
+          extras,
+          publicClient,
+        })
+      })
+
+      describe('with :exact (collateral token)', () => {
+        it('applies the collateral token decimals to user inputs', async () => {
+          expect(minimumPayoutAmount).toEqual(42069)
+        })
+      })
+    })
+
     describe('with :internal', () => {
-      const USDC_SEPOLIA = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238'
       const args = ['18', '420', '69', USDC_SEPOLIA]
       const formattedInputs = [
         {
