f24c6b67325f37572ba91ce001c56f03ebabbc02
fabianschu <48454910+fabianschu@users.noreply.github.com>
2024-05-25 13:03:49 +0200

    chore: adds test for AUT_TokenGated_Roles_v1

--- tests/getDeploy/getDeploy.test.ts
+++ tests/getDeploy/getDeploy.test.ts
@@ -11,6 +11,76 @@ import { isAddress } from 'viem'
 
 describe('#getDeploy', () => {
   const { publicClient, walletClient } = getTestConnectors()
+  const expectedBaseInputSchema = {
+    orchestrator: {
+      name: 'Orchestrator_v1',
+      inputs: [
+        {
+          name: 'owner',
+          type: 'address',
+          jsType: '0xstring',
+          description: 'The owner address of the workflow',
+        },
+        {
+          name: 'token',
+          type: 'address',
+          jsType: '0xstring',
+          description: 'The payment token associated with the workflow',
+        },
+      ],
+    },
+    fundingManager: {
+      name: 'FM_Rebasing_v1',
+      inputs: [
+        {
+          name: 'orchestratorTokenAddress',
+          type: 'address',
+          jsType: '0xstring',
+          description:
+            'The address of the token that will be deposited to the funding manager',
+        },
+      ],
+    },
+    authorizer: {
+      name: 'AUT_Roles_v1',
+      inputs: [
+        {
+          name: 'initialOwner',
+          type: 'address',
+          jsType: '0xstring',
+          description: 'The initial owner of the workflow',
+        },
+        {
+          name: 'initialManager',
+          type: 'address',
+          jsType: '0xstring',
+          description: 'The initial manager of the workflow',
+        },
+      ],
+    },
+    paymentProcessor: {
+      inputs: [],
+      name: 'PP_Simple_v1',
+    },
+  }
+
+  const args: GetUserArgs<{
+    fundingManager: 'FM_Rebasing_v1'
+    authorizer: 'AUT_Roles_v1'
+    paymentProcessor: 'PP_Simple_v1'
+  }> = {
+    orchestrator: {
+      owner: '0x5eb14c2e7D0cD925327d74ae4ce3fC692ff8ABEF',
+      token: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
+    },
+    fundingManager: {
+      orchestratorTokenAddress: '0x5eb14c2e7D0cD925327d74ae4ce3fC692ff8ABEF',
+    },
+    authorizer: {
+      initialOwner: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
+      initialManager: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
+    },
+  }
 
   describe('Modules: FM_Rebasing_v1, AUT_Roles_v1, PP_Simple_v1', () => {
     const requestedModules = {
@@ -18,77 +88,6 @@ describe('#getDeploy', () => {
       paymentProcessor: 'PP_Simple_v1',
       authorizer: 'AUT_Roles_v1',
     } satisfies RequestedModules
-
-    const expectedBaseInputSchema = {
-      orchestrator: {
-        name: 'Orchestrator_v1',
-        inputs: [
-          {
-            name: 'owner',
-            type: 'address',
-            jsType: '0xstring',
-            description: 'The owner address of the workflow',
-          },
-          {
-            name: 'token',
-            type: 'address',
-            jsType: '0xstring',
-            description: 'The payment token associated with the workflow',
-          },
-        ],
-      },
-      fundingManager: {
-        name: 'FM_Rebasing_v1',
-        inputs: [
-          {
-            name: 'orchestratorTokenAddress',
-            type: 'address',
-            jsType: '0xstring',
-            description:
-              'The address of the token that will be deposited to the funding manager',
-          },
-        ],
-      },
-      authorizer: {
-        name: 'AUT_Roles_v1',
-        inputs: [
-          {
-            name: 'initialOwner',
-            type: 'address',
-            jsType: '0xstring',
-            description: 'The initial owner of the workflow',
-          },
-          {
-            name: 'initialManager',
-            type: 'address',
-            jsType: '0xstring',
-            description: 'The initial manager of the workflow',
-          },
-        ],
-      },
-      paymentProcessor: {
-        inputs: [],
-        name: 'PP_Simple_v1',
-      },
-    }
-
-    const args: GetUserArgs<{
-      fundingManager: 'FM_Rebasing_v1'
-      authorizer: 'AUT_Roles_v1'
-      paymentProcessor: 'PP_Simple_v1'
-    }> = {
-      orchestrator: {
-        owner: '0x5eb14c2e7D0cD925327d74ae4ce3fC692ff8ABEF',
-        token: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
-      },
-      fundingManager: {
-        orchestratorTokenAddress: '0x5eb14c2e7D0cD925327d74ae4ce3fC692ff8ABEF',
-      },
-      authorizer: {
-        initialOwner: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
-        initialManager: '0x7AcaF5360474b8E40f619770c7e8803cf3ED1053',
-      },
-    }
 
     describe('optionalModules: none', () => {
       describe('inputs', () => {
@@ -304,6 +303,49 @@ describe('#getDeploy', () => {
               },
             },
           })
+          expect(isAddress(simulationResult.result as string)).toBeTrue
+        })
+      })
+    })
+  })
+
+  describe('Modules: FM_Rebasing_v1, AUT_TokenGated_Roles_v1, PP_Simple_v1', () => {
+    const requestedModules = {
+      fundingManager: 'FM_Rebasing_v1',
+      paymentProcessor: 'PP_Simple_v1',
+      authorizer: 'AUT_TokenGated_Roles_v1',
+    } satisfies RequestedModules
+
+    const expectedInputSchema = {
+      ...expectedBaseInputSchema,
+      authorizer: {
+        ...expectedBaseInputSchema.authorizer,
+        name: 'AUT_TokenGated_Roles_v1',
+      },
+    }
+
+    describe('optionalModules: none', () => {
+      describe('inputs', () => {
+        it('has the correct format', async () => {
+          const { inputs } = await getDeploy(
+            publicClient,
+            walletClient,
+            requestedModules
+          )
+          expect(inputs).toEqual({
+            ...expectedInputSchema,
+          } as any)
+        })
+      })
+
+      describe('simulate', () => {
+        it('returns the orchestrator address as result', async () => {
+          const { simulate } = await getDeploy(
+            publicClient,
+            walletClient,
+            requestedModules
+          )
+          const simulationResult = await simulate(args)
           expect(isAddress(simulationResult.result as string)).toBeTrue
         })
       })
