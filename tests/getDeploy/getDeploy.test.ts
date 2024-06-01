c8b4d6999dd7b384cee8c3bcf422debbc9e555ab
fabianschu <48454910+fabianschu@users.noreply.github.com>
2024-06-01 12:31:07 +0200

    chore: add `testHelpers` dir to tests

--- tests/getDeploy/getDeploy.test.ts
+++ tests/getDeploy/getDeploy.test.ts
@@ -1,6 +1,6 @@
 import { expect, describe, it } from 'bun:test'
 
-import { getTestConnectors } from '../getTestConnectors'
+import { getTestConnectors } from '../testHelpers/getTestConnectors'
 import { getDeploy } from '../../src'
 import { Hex, isAddress } from 'viem'
 
---
+++ tests/getModule/getRun.test.ts
@@ -0,0 +1,9 @@
+import { expect, describe, it } from 'bun:test'
+
+import getModule from '../../src/getModule'
+import { getTestConnectors } from '../testHelpers/getTestConnectors'
+import utils from '../../tools'
+
+describe('#getRun', () => {
+  it('')
+})
