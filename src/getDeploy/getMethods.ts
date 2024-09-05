fbdafb650ab1d4fb8f843b926698b377ac43a273
mguleryuz <mguleryuz3@gmail.com>
2024-09-05 13:21:10 -0500

    refactor: approve estimateGas and simulate at getDeploy

--- src/getDeploy/getMethods.ts
+++ src/getDeploy/getMethods.ts
@@ -1,11 +1,11 @@
 import { formatEther, type PublicClient } from 'viem'
-import {
-  type EstimateGasReturn,
-  type FactoryType,
-  type GetUserArgs,
-  type Inverter,
-  type PopWalletClient,
-  type RequestedModules,
+import type {
+  DeployMethodKind,
+  FactoryType,
+  GetUserArgs,
+  Inverter,
+  PopWalletClient,
+  RequestedModules,
 } from '..'
 import { getViemMethods, handlePimFactoryApprove } from './utils'
 import getArgs from './getArgs'
@@ -33,7 +33,6 @@ export default async function getMethods<
     default: () => getModuleData('OrchestratorFactory_v1').abi,
   }[factoryType]()
 
-  // Get the methods from the Viem handler
   const {
     factoryAddress,
     write,
@@ -47,28 +46,12 @@ export default async function getMethods<
     version: 'v1.0.0',
   })
 
-  // Simulate the deployment
-  const simulate = async (userArgs: GetUserArgs<T, FT>) => {
-    try {
-      const arr = await getArgs({ userArgs, kind: 'simulate', ...params })
+  type Args = GetUserArgs<T, FT>
 
-      const res = await simulateWrite(arr, {
-        account: walletClient.account.address,
-      })
-
-      const orchestratorAddress = res.result as `0x${string}`
-
-      return {
-        result: orchestratorAddress,
-        request: res.request,
-      }
-    } catch (e: any) {
-      throw handleError({ requestedModules, error: e })
-    }
-  }
-
-  // Run the deployment = write
-  const run = async (userArgs: GetUserArgs<T, FT>) => {
+  async function handleDeployment<K extends DeployMethodKind>(
+    userArgs: Args,
+    kind: K
+  ) {
     try {
       await handlePimFactoryApprove({
         factoryType,
@@ -78,46 +61,57 @@ export default async function getMethods<
         userArgs,
       })
 
-      const arr = await getArgs({ userArgs, kind: 'write', ...params })
+      const arr = await getArgs({ userArgs, kind, ...params })
 
-      const simulationRes = await simulateWrite(arr, {
-        account: walletClient.account.address,
-      })
-
-      const orchestratorAddress = simulationRes.result as `0x${string}`
-      const transactionHash = await write(arr, {} as any)
-
-      return {
-        orchestratorAddress,
-        transactionHash,
+      const actions = {
+        simulate: async () => {
+          const res = await simulateWrite(arr, {
+            account: walletClient.account.address,
+          })
+          const orchestratorAddress = res.result as `0x${string}`
+          return {
+            result: orchestratorAddress,
+            request: res.request,
+          }
+        },
+        estimateGas: async () => {
+          const value = String(
+            await esitmateGasOrg(arr, {
+              account: walletClient.account.address,
+            })
+          )
+          const formatted = formatEther(BigInt(value))
+          return {
+            value,
+            formatted,
+          }
+        },
+        write: async () => {
+          const simulationRes = await simulateWrite(arr, {
+            account: walletClient.account.address,
+          })
+          const orchestratorAddress = simulationRes.result as `0x${string}`
+          const transactionHash = await write(arr, {} as any)
+          return {
+            orchestratorAddress,
+            transactionHash,
+          }
+        },
       }
+
+      const selected = actions[kind]
+
+      const res = (await selected()) as Awaited<ReturnType<typeof selected>>
+
+      return res
     } catch (e: any) {
       throw handleError({ requestedModules, error: e })
     }
   }
 
-  // Estimate the gas for the deployment
-  const estimateGas = async (
-    userArgs: GetUserArgs<T, FT>
-  ): Promise<EstimateGasReturn> => {
-    try {
-      const arr = await getArgs({ userArgs, kind: 'estimateGas', ...params })
-
-      const value = String(
-        await esitmateGasOrg(arr, {
-          account: walletClient.account.address,
-        })
-      )
-      const formatted = formatEther(BigInt(value))
-
-      return {
-        value,
-        formatted,
-      }
-    } catch (e: any) {
-      throw handleError({ requestedModules, error: e })
-    }
+  return {
+    run: (userArgs: Args) => handleDeployment(userArgs, 'write'),
+    simulate: (userArgs: Args) => handleDeployment(userArgs, 'simulate'),
+    estimateGas: (userArgs: Args) => handleDeployment(userArgs, 'estimateGas'),
   }
-
-  return { run, simulate, estimateGas }
 }
