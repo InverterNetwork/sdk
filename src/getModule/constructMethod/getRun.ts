a59dc5fd1e9d61b52523b16aa8d3408b79a55b6e
mguleryuz <mguleryuz3@gmail.com>
2024-09-05 15:28:09 -0500

    refactor: approve estimateGas and simulate at getModule

--- src/getModule/constructMethod/getRun.ts
+++ src/getModule/constructMethod/getRun.ts
@@ -1,4 +1,4 @@
-import { processInputs, formatOutputs, ERC20_ABI } from '@/utils'
+import { processInputs, formatOutputs, processAllowances } from '@/utils'
 import { Inverter } from '@/Inverter'
 
 import type {
@@ -6,7 +6,6 @@ import type {
   GetMethodArgs,
   PopPublicClient,
   PopWalletClient,
-  RequiredAllowances,
   MethodKind,
   GetMethodResponse,
   PopContractReturnType,
@@ -14,50 +13,6 @@ import type {
 import { formatEther } from 'viem'
 import { handleError } from '../../utils'
 import type { ExtendedAbiParameter } from '@inverter-network/abis'
-
-const runDependencies = async ({
-  requiredAllowances,
-  publicClient,
-  walletClient,
-}: {
-  requiredAllowances: RequiredAllowances[]
-  publicClient: PopPublicClient
-  walletClient?: PopWalletClient
-}) => {
-  if (!walletClient) return
-  const dependencyTxHashes = await Promise.all(
-    requiredAllowances.map((requiredAllowance) => {
-      return walletClient.writeContract({
-        address: requiredAllowance.token,
-        account: walletClient.account,
-        functionName: 'approve',
-        args: [requiredAllowance.spender, requiredAllowance.amount],
-        abi: ERC20_ABI,
-      })
-    })
-  )
-
-  return await Promise.all(
-    dependencyTxHashes.map((hash) => {
-      return publicClient.waitForTransactionReceipt({ hash })
-    })
-  )
-}
-
-const runWithDependencies = async ({
-  mainArgs,
-  mainFunction,
-  ...params
-}: {
-  mainFunction: any
-  mainArgs: any
-  requiredAllowances: RequiredAllowances[]
-  publicClient: PopPublicClient
-  walletClient?: PopWalletClient
-}) => {
-  await runDependencies(params)
-  return mainFunction(mainArgs)
-}
 
 // Construct the run function
 export default function getRun<
@@ -100,17 +55,20 @@ export default function getRun<
       kind,
     })
 
-    const hasDependencies = requiredAllowances.find(
-      (requiredAllowance) => requiredAllowance.amount > 0n
-    )
-
     /**
      * Resposne data based on the kind of method
      * catch and try decode the error
      */
-    const res = await (() => {
+    const res = await (async () => {
       try {
-        const selectedRes = {
+        if (kind !== 'read')
+          await processAllowances({
+            requiredAllowances,
+            publicClient,
+            walletClient,
+          })
+
+        const actions = {
           simulate: async () => {
             // if no dependencies, run the simulate function
             const simRes = await contract.simulate[name](processedInputs, {
@@ -122,21 +80,8 @@ export default function getRun<
 
             return simRes
           },
-          write: async () => {
-            if (hasDependencies)
-              return await runWithDependencies({
-                mainFunction: contract.write[name],
-                mainArgs: processedInputs,
-                requiredAllowances,
-                publicClient,
-                walletClient,
-              })
-
-            return await contract.write[name](processedInputs)
-          },
-          read: async () => {
-            return await contract.read[name](processedInputs)
-          },
+          write: () => contract.write[name](processedInputs),
+          read: () => contract.read[name](processedInputs),
           estimateGas: async () => {
             const value = await contract.estimateGas[name](processedInputs)
             const formatted = formatEther(value)
@@ -146,9 +91,13 @@ export default function getRun<
             }
             return result
           },
-        }[kind]()
+        }
 
-        return selectedRes
+        const selected = actions[kind]
+
+        const res = (await selected()) as ReturnType<typeof selected>
+
+        return res as any
       } catch (e: any) {
         throw handleError({ abi: contract.abi, error: e })
       }
