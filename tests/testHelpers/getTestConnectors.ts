c4962f34e913e339328cf02ab266d14b5b20da26
mguleryuz <mguleryuz3@gmail.com>
2024-09-18 12:40:26 -0500

    chore: initial setup

--- tests/testHelpers/getTestConnectors.ts
+++ tests/testHelpers/getTestConnectors.ts
@@ -2,46 +2,43 @@ import {
   createPublicClient,
   http,
   createWalletClient,
-  type Chain,
-  type Transport,
+  // type Chain,
+  // type Transport,
 } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import * as chains from 'viem/chains'
 
-import { optimismSepolia as org } from 'viem/chains'
+import { sepolia } from 'viem/chains'
 
-export const optimismSepolia = {
-  ...org,
-  ...(true && {
-    rpcUrls: {
-      default: {
-        http: [
-          `https://optimism-sepolia.infura.io/v3/${'a54805c2acb44bb680d4770edfe62452'}`,
-        ],
-      },
+export const local = {
+  ...sepolia,
+  rpcUrls: {
+    default: {
+      http: [`http://127.0.0.1:8545`],
     },
-  }),
-}
+  },
+} as unknown as typeof sepolia
 
-type ChainKey = keyof typeof chains
+export type ChainKey = keyof typeof chains
 
 const privKey = process.env['TEST_PRIVATE_KEY'] as `0x${string}` | undefined
 
 if (!privKey) throw new Error('Error: please add priv key to .env')
 
-export const getTestConnectors = (chainKey?: ChainKey) => {
-  const chain = <Chain>(chainKey ? chains[chainKey] : optimismSepolia)
+export const getTestConnectors = () => {
+  // const chain = <Chain>(chainKey ? chains[chainKey] : optimismSepolia)
+  const chain = local
   // Public Client: This is used to read from the blockchain.
   const publicClient = createPublicClient({
     chain,
-    transport: http() as Transport,
+    transport: http(),
   })
 
   // Wallet Client used to write data to the blockchain
   const walletClient = createWalletClient({
     account: privateKeyToAccount(privKey),
     chain,
-    transport: http() as Transport,
+    transport: http(),
   })
 
   return {
---
+++ tools/preTest.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+# Global variables
+INSTALL_URL="https://foundry.paradigm.xyz"
+DEPS=("curl" "make" "forge" "anvil")
+DEPLOY_SCRIPT="script/deploymentScript/TestnetDeploymentScript.s.sol"
+ENV_FILE=".env"
+DEPLOYER_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+TEST_PRIVATE_KEY="TEST_PRIVATE_KEY=$DEPLOYER_PRIVATE_KEY"
+RPC_URL="http://127.0.0.1:8545"
+
+# Function to detect OS
+detect_os() {
+    local os
+    os=$(uname)
+    if [[ "$os" == "Darwin" ]]; then
+        printf "Detected macOS 🍏\n"
+    elif [[ "$os" == "Linux" ]]; then
+        printf "Detected Linux 🐧\n"
+    else
+        printf "Unsupported OS detected ❌\n" >&2
+        return 1
+    fi
+}
+
+# Function to check if dependencies are installed
+check_dependencies() {
+    printf "Checking for required dependencies 🔍\n"
+    for dep in "${DEPS[@]}"; do
+        if ! command -v "$dep" &>/dev/null; then
+            printf "$dep is not installed ❌\n"
+            install_dependency "$dep"
+        else
+            printf "$dep is already installed ✅\n"
+        fi
+    done
+}
+
+# Function to install dependencies
+install_dependency() {
+    local dep="$1"
+    printf "Attempting to install $dep ⚙️\n"
+    case "$dep" in
+    curl)
+        if [[ "$(uname)" == "Darwin" ]]; then
+            brew install curl || return 1
+        else
+            sudo apt-get install -y curl || return 1
+        fi
+        ;;
+    make)
+        if [[ "$(uname)" == "Darwin" ]]; then
+            xcode-select --install || return 1
+        else
+            sudo apt-get install -y build-essential || return 1
+        fi
+        ;;
+    forge | anvil)
+        printf "Installing Foundry 🛠️\n"
+        if ! curl -L $INSTALL_URL | bash; then
+            printf "Failed to install Foundry ❌\n" >&2
+            return 1
+        fi
+        source "$HOME/.bashrc" || source "$HOME/.zshrc"
+        ;;
+    *)
+        printf "Unknown dependency $dep ❓\n" >&2
+        return 1
+        ;;
+    esac
+    printf "$dep installation completed 🚀\n"
+}
+
+# Function to run anvil
+start_anvil() {
+    printf "Starting Anvil blockchain simulator 🔥\n"
+    pkill anvil
+    anvil &
+    ANVIL_PID=$!
+    export ANVIL_PID
+}
+
+# Function to handle contract installation
+install_contracts() {
+    printf "Running 'make install' ⚙️\n"
+    if ! make install; then
+        printf "Contract installation failed ❌\n" >&2
+        return 1
+    fi
+    printf "Inverter Protocol Installation Complete 🚀\n"
+}
+
+# Function to deploy the protocol
+deploy_protocol() {
+    printf "Starting Protocol Deployment 🚀\n"
+    source dev.env || {
+        printf "Failed to source dev.env ❌\n" >&2
+        return 1
+    }
+    DEPLOYER_PRIVATE_KEY="$DEPLOYER_PRIVATE_KEY" forge script "$DEPLOY_SCRIPT" --rpc-url "$RPC_URL" --broadcast --silent >/dev/null 2>&1 ||
+        {
+            printf "Deployment failed ❌\n" >&2
+            return 1
+        }
+    printf "Deployment successful 🚀\n"
+}
+
+# Function to update .env file
+update_env() {
+    printf "Checking and updating .env file 🔑\n"
+    if [[ ! -f "$ENV_FILE" ]]; then
+        printf ".env file does not exist, creating it 🛠️\n"
+        echo "$TEST_PRIVATE_KEY" >"$ENV_FILE"
+        printf "Added TEST_PRIVATE_KEY to new .env file 📄\n"
+    else
+        if grep -q "TEST_PRIVATE_KEY" "$ENV_FILE"; then
+            printf "Updating existing TEST_PRIVATE_KEY value in .env 🔄\n"
+            sed -i'' -e "s/^TEST_PRIVATE_KEY=.*/$TEST_PRIVATE_KEY/" "$ENV_FILE"
+            printf "TEST_PRIVATE_KEY updated in .env ✅\n"
+        else
+            printf "Adding TEST_PRIVATE_KEY to .env 🆕\n"
+            echo "$TEST_PRIVATE_KEY" >>"$ENV_FILE"
+            printf "TEST_PRIVATE_KEY added to .env file ✅\n"
+        fi
+    fi
+}
+
+# Main function
+main() {
+    detect_os || {
+        printf "OS detection failed ❌\n"
+        exit 1
+    }
+    check_dependencies || {
+        printf "Dependency check failed ❌\n"
+        exit 1
+    }
+
+    start_anvil || {
+        printf "Failed to start Anvil ❌\n"
+        exit 1
+    }
+
+    printf "Entering contracts directory 📂\n"
+    cd ./contracts || {
+        printf "Failed to enter ./contracts ❌\n" >&2
+        return 1
+    }
+
+    install_contracts || {
+        printf "Contract installation failed ❌\n"
+        exit 1
+    }
+
+    deploy_protocol || {
+        printf "Protocol deployment failed ❌\n"
+        exit 1
+    }
+
+    printf "Returning to the root directory 📂\n"
+    cd .. || {
+        printf "Failed to return to root directory ❌\n" >&2
+        return 1
+    }
+
+    update_env || {
+        printf "Failed to update .env ❌\n"
+        exit 1
+    }
+
+    printf "Testnet protocol deployment is done 🚀. Press CTRL+C to end the session! 🖱️\n"
+
+    # Keep Anvil process running and display its logs ( disabled for pre post test )
+    # wait $ANVIL_PID
+}
+
+# Execute the main function
+main
