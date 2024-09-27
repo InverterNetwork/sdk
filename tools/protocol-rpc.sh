c2fe1431196ddea0a57e45cd32bc141df326e19a
mguleryuz <mguleryuz3@gmail.com>
2024-09-27 12:33:20 -0500

    chore: add raw script comment for manual test

--- tools/protocol-rpc.sh
+++ tools/protocol-rpc.sh
@@ -238,3 +238,5 @@ main() {
 }
 
 main "$@"
+
+# RAW SCRIPT CMD: source dev.env && DEPLOYER_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" forge script script/deploymentScript/TestnetDeploymentScript.s.sol --rpc-url http://127.0.0.1:8545 --broadcast -v
