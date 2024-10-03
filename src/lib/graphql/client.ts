ac71bad8681cfb2b0d96ed45d9feacccb22c6761
mguleryuz <mguleryuz3@gmail.com>
2024-10-03 11:10:38 -0500

    fix: ws url error at graphql client

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -7,7 +7,10 @@ import {
 import { DEFAULT_GRAPHQL_URL } from './constants'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 
-const subscriptionClient = new SubscriptionClient(DEFAULT_GRAPHQL_URL, {})
+// Convert HTTPS URL to WebSocket URL
+const WEBSOCKET_URL = DEFAULT_GRAPHQL_URL.replace('https', 'wss')
+
+const subscriptionClient = new SubscriptionClient(WEBSOCKET_URL, {})
 
 const client = new Client({
   url: DEFAULT_GRAPHQL_URL,
