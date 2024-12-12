de17ce51dcf855ae8dd1d2d5165376dca26d0081
mguleryuz <mguleryuz3@gmail.com>
2024-12-12 09:46:10 +0100

    feat: blacklist issuance token deploy

---
+++ tests/e2e/deploy/ERC20IssuanceBlacklist.test.ts
@@ -0,0 +1,44 @@
+import { expect, describe, it } from 'bun:test'
+
+import { sdk } from 'tests/helpers'
+
+describe('#DEPLOY_ERC20_ISSUANCE_BLACKLIST', async () => {
+  const deployer = sdk.walletClient.account.address
+  let tokenAddress: `0x${string}`
+
+  it('1. Should Deploy The ERC20 Issuance Blacklist Contract', async () => {
+    const data = await sdk.deploy({
+      name: 'ERC20Issuance_Blacklist_v1',
+      args: {
+        name: 'My Token',
+        symbol: 'MT',
+        decimals: 18,
+        maxSupply: '1000000',
+        initialAdmin: deployer,
+        initialBlacklistManager: deployer,
+      },
+    })
+
+    if (!data.contractAddress) throw new Error('Token Address Not Found')
+
+    tokenAddress = data.contractAddress
+
+    expect(data).toContainKeys(['contractAddress', 'transactionHash'])
+  })
+
+  it('2. Should getModule && Mint Tokens', async () => {
+    const module = sdk.getModule({
+      address: tokenAddress,
+      name: 'ERC20Issuance_v1',
+      extras: {
+        decimals: 18,
+      },
+    })
+
+    expect(module).toBeObject()
+
+    const hash = await module.write.mint.run([deployer, '1000'])
+
+    expect(hash).toBeString()
+  })
+})
